20e1dcf2bcc52a6b1fb0ddba70051e62e5d505f5
Naeem <naeem.vdc@gmail.com>
2021-07-06 15:38:12 +0530

    Add Company name in Checkout

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,17 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/shipping-address/address-renderer/default.html':
+                'Printq_CheckoutAddCompanyName/template/shipping-address/address-renderer/default.html',
+
+            'Magento_Checkout/template/shipping-information/address-renderer/default.html':
+                'Printq_CheckoutAddCompanyName/template/shipping-information/address-renderer/default.html',
+
+            'Magento_Checkout/template/billing-address/details.html':
+                'Printq_CheckoutAddCompanyName/template/billing-address/details.html',
+
+             'Magento_Checkout/js/view/billing-address/list':
+                'Printq_CheckoutAddCompanyName/js/view/billing-address/list',
+  	     }
+    }
+};	
